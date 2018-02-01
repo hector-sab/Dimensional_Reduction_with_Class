ec5592406bcccd8eef26064a4bf4e8607e38a0b0
Héctor Sab <hector.sab@hotmail.com>
2018-02-01 14:09:06 -0600

    .

--- models.py
+++ models.py
@@ -199,6 +199,7 @@ class SegModel:
     
     for it in range(int(num_ex/bs)):
       data = dataset.next_batch(bs)
+      print(data['ims'].shape)
       feed_dict = {self.x: data['ims'],
                    self.y_seg: data['seg']}
       acc = self.session.run(self.accuracy,feed_dict=feed_dict)
