40c9d1a6eb12a3e9d27709d06265d1926ae53155
Héctor Sab <hector.sab@hotmail.com>
2018-02-02 16:26:12 -0600

    .

--- models.py
+++ models.py
@@ -569,29 +569,34 @@ class ModelStv1:
       relu=True,dropout=self.dropout,drop_prob=self.drop_prob,
       histogram=histogram,name='deconv1')
 
-    d2_shape = [ks5,ks5,num_k4,num_k5]
+    #d2_shape = [ks5,ks5,num_k4,num_k5]
+    d2_shape = [ks5,ks5,num_k5,num_k5]
     self.deconv2 = ut.deconv2(inp=self.deconv1,shape=c5_shape,
       relu=True,name='deconv2',dropout=self.dropout,
       drop_prob=self.drop_prob,histogram=histogram)
 
     self.sum1 = self.deconv2 + self.conv4
-    d3_shape = [ks4,ks4,num_k3,num_k4]
+    #d3_shape = [ks4,ks4,num_k3,num_k4]
+    d3_shape = [ks4,ks4,num_k4,num_k5]
     self.deconv3 = ut.deconv2(inp=self.sum1,shape=c4_shape,
       relu=True,strides=[1,2,2,1],name='deconv3',
       dropout=self.dropout,drop_prob=self.drop_prob,histogram=histogram)
 
-    d4_shape = [ks3,ks3,num_k2,num_k3]
+    #d4_shape = [ks3,ks3,num_k2,num_k3]
+    d4_shape = [ks3,ks3,num_k3,num_k4]
     self.deconv4 = ut.deconv2(inp=self.deconv3,shape=c3_shape,
       relu=True,name='deconv4',dropout=self.dropout,
       drop_prob=self.drop_prob,histogram=histogram)
 
     self.sum2 = self.deconv4 + self.conv2
-    d5_shape = [ks2,ks2,num_k2,num_k2]
+    #d5_shape = [ks2,ks2,num_k2,num_k2]
+    d5_shape = [ks2,ks2,num_k1,num_k3]
     self.deconv5 = ut.deconv2(inp=self.sum2,shape=c2_shape,
       strides=[1,2,2,1],relu=True,name='deconv5',dropout=self.dropout,
       drop_prob=self.drop_prob,histogram=histogram)
 
-    d6_shape = [ks1,ks1,self.num_class,num_k2]
+    #d6_shape = [ks1,ks1,self.num_class,num_k2]
+    d6_shape = [ks1,ks1,self.num_class,num_k1]
     self.deconv6 = ut.deconv2(inp=self.deconv5,shape=d6_shape,
       relu=False,name='deconv6',histogram=histogram)
 
