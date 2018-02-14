0e34c67d7200e08e9900ac8a4bce5eafbaad6275
Héctor Sab <hector.sab@hotmail.com>
2018-02-14 10:15:01 -0600

    .

--- seg_model.py
+++ seg_model.py
@@ -462,13 +462,17 @@ class SegModelSigmoid:
     ##
     #####-S: Creates model
     if model==0:
-      self.model = md.MaxPoolNoSC(inp=self.x,ex=self.ex,histogram=histogram)
+      self.model = md.MaxPoolNoSC(inp=self.x,ex=self.ex,histogram=histogram,
+        version=version)
     elif model==1:
-      self.model = md.MaxPoolSC(inp=self.x,ex=self.ex,histogram=histogram)
+      self.model = md.MaxPoolSC(inp=self.x,ex=self.ex,histogram=histogram,
+        version=version)
     elif model==2:
-      self.model = md.StrideNoSC(inp=self.x,histogram=histogram)
+      self.model = md.StrideNoSC(inp=self.x,histogram=histogram,
+        version=version)
     elif model==3:
-      self.model = md.StrideSC(inp=self.x,histogram=histogram)
+      self.model = md.StrideSC(inp=self.x,histogram=histogram,
+        version=version)
 
 
 
